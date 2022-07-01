fe5fed13a29f5807463bb1d7f05d2f6c70a6d089
P Govinda Rajulu <g.pichiguntla@f5.com>
2022-07-01 17:34:03 +0530

    Preorder traversal

---
+++ binaryTree/preorderTraversal.go
@@ -0,0 +1,46 @@
+/*
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+	var root *TreeNode
+	result := preOrderTraversal(root)
+	fmt.Printf("Result:%v", result)
+}
+
+func preOrderTraversal(root *TreeNode) []int {
+	var ans []int
+	traversalTree(root, &ans)
+	return ans
+}
+
+func traversalTree(root *TreeNode, ans *[]int) {
+	if root == nil {
+		return
+	}
+
+	*ans = append(*ans, root.Val)
+	traversalTree(root.Left, ans)
+	traversalTree(root.Right, ans)
+
+}
